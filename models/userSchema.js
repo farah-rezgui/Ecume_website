46308865e55c3e0226768bf06e15a21294f342b6
farah-rezgui <rezguifarah126@gmail.com>
2025-03-17 09:46:47 +0000

    etat

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require ("bcrypt");
 const userSchema = new mongoose.schema ({
-    nom : { type : 'string' , required : true },
+    nom : { type : string , required : true },
     prenom : String ,
-    email : {type : ' string' , required : true  , unique : tre },
-    password : { type : 'string' , required : true },
-    rele : { type : 'string' , enum : ['admin','client'] },
+    email : {type : string , required : true  , unique : tre },
+    password : { type : string , required : true },
+    rele : { type : string , enum : ['admin','client'] },
+    etat : {type: Boolean},
 }, {timestamps : true});
 
 userSchema.pre('save', async function(req , res ,next) {
@@ -13,6 +14,7 @@ userSchema.pre('save', async function(req , res ,next) {
         const salt = await bcrypt.genSalt();
         const user = this ;
         user.password = await bcrypt.hash(user.password, salt);
+        user.etat=false;
         next();
     } catch (err) {
         next(err);

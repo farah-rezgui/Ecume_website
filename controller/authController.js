fd5d9d0beebcea4edfe55b9bf877b01e9895ab98
farah-rezgui <rezguifarah126@gmail.com>
2025-03-20 23:14:42 +0000

    delete

--- controller/authController.js
+++ controller/authController.js
@@ -9,6 +9,17 @@ module.exports.getAllUsers = async (req , res) =>{
         res.status(500).json ({message: error.message})
     }
 }
+module.exports.getUserById = async (req , res) =>{
+    try {
+        console.log(req.params.id);
+        const {id} = req.params;
+        const id1 = req.params.id;
+        const usersList = await userModel.findById(id);
+        res.status(200).json({usersList})
+    } catch (error) {
+        res.status(500).json ({message: error.message})
+    }
+}
 module.exports.addUserClient = async (req , res) =>{
     try {
         console.log(req.body);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const authController = require('../controller/authController');
 /* GET users listing. */
 router.get('/getAllUsers', authController.getAllUsers); 
-router.post('/addUserClient', authController.addUserClient); 
+router.get('/getUserById/:id', authController.getUserById); 
+router.post('/addUserClient', authController.addUserClient);
+router.post('/deleteUser/:id',);  
 
 
 module.exports = router;
